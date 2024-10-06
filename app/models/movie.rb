a25f4690b01abbd752a9bc3da571c18ffeb2c299
nhat-nguyen-tamu <nmnguy29@tamu.edu>
2024-10-06 17:48:46 -0500

    fixed date issue, added validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ActiveRecord::Base
+    validates :title, presence: true
+    validates :rating, presence: true
+    validates :release_date, presence: true
+
+    validate :date_valid
+
+    def date_valid
+        return if release_date.blank?
+
+        begin
+            Date.parse(release_date.strftime("%b %e, %y"))
+        rescue ArgumentError
+            errors.add(:release_date, "isn't a valid date, please use MM-DD-YYYY")
+        end
+    end
 end
